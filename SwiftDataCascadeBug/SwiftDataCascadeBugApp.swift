d1d158880e71a31af47b4cd165d81ab706ef45c6
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-11-23 19:51:05 -0800

    Initial Commit

---
+++ SwiftDataCascadeBug/SwiftDataCascadeBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataCascadeBugApp.swift
+//  SwiftDataCascadeBug
+//
+//  Created by Nikolai Nobadi on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataCascadeBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
